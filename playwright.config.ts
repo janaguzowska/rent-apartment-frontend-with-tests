024f17b1115be3b473c32bf3deff5458c275af64
Ania <ania.lanskaya@gmail.com>
2026-04-17 20:46:49 +0200

    playwright without committed webserver config

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,12 +74,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run preview -- --port 5173',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-    stdout: 'pipe',
-    stderr: 'pipe',
-  },
+  // webServer: {
+  //   command: 'npm run preview -- --port 5173',
+  //   url: 'http://localhost:5173',
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120 * 1000,
+  //   stdout: 'pipe',
+  //   stderr: 'pipe',
+  // },
 });
